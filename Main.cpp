ee0600af8ce60d40123be5f1a31b33d76f6e037f
Robotophile <ksyunya.iv@gmail.com>
2024-10-19 17:54:24 +0300

    add imput check, add functions into Main.cpp

--- Main.cpp
+++ Main.cpp
@@ -4,7 +4,19 @@
 int main(){
     size_t M = 0, N = 0;
     std::cin >> M >> N;
-    int ** t = ct(M,N);
-    std::cout << M << "" << N << '\n';
-    
+    int** t = nullptr;
+    if (std::cin.fail()){
+        std::cout << "Input error" << '\n';
+    }
+
+    try{
+        t = ct(M,N);
+    }catch (const std::bad_alloc &e){
+        std::cout << "Out of memory" << '\n';
+        return 1;
+    }
+    in(t, M, N);
+    out(t, M, N);
+    dt(t, M);
+    std::cout << '\n';
 }
